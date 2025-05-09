106a2b04200d5318de34902f6b1a1929b3d99283
Leonardo Soteldo <leonardo@imac>
2025-05-09 19:45:31 -0400

    Added functionality for including notes in the export

--- kindle_clips.py
+++ kindle_clips.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-import datetime as dt
-import re
+from datetime import date, time
+from re import search, IGNORECASE
 import argparse
 
 MONTHS: dict = {
@@ -19,31 +19,36 @@ MONTHS: dict = {
 }
 
 @dataclass
-class Clip:
+class RawClip:
     source: str
     info: str
+    blank: str
     content: str
+    delimiter: str
 
 @dataclass
-class Highlight:
+class Clip:
+    type: str
     source: str
     page: list[int]
     location: list[int]
-    date: dt.date | None
-    time: dt.time | None
+    date: date | None
+    time: time | None
     content: str
 
 @dataclass
-class Report:
-    notes_cnt: int
-    highlights_cnt: int
-    highlights: list[Highlight]
+class Extraction:
+    highlights: list[Clip]
+    notes: list[Clip]
 
 ### CLIPS PARSING
 ######################################################################
 
-def parse_clips_file(file: str) -> Report:
-    """Parse a given file (a path in the form of a str) into a Report.
+# TODO: add functionality for notes also.
+# TODO: add basic sorting for output
+
+def parse_rawclips_file(file: str) -> Extraction:
+    """Parse a given file (a path in the form of a str) into a Extraction.
 
     Every kindle clip, as existing in the kindle's 'My Clippings.txt'
     file, consists in:
@@ -54,11 +59,10 @@ def parse_clips_file(file: str) -> Report:
       - a delimiter line made by repeated equal signs: '=========='
 
     Every line is read, stripped of the newline character, and stored
-    as their correspondent Clip's field (the blank and delimiter lines
-    are discarded). Then the Clip is evaluated to check if It's a valid
-    highlight, discarding it if It's a note. Finally, the Clip is
-    passed to the 'parse_clip()' function and stored as a Highlight in
-    a list to be returned as part or the output Report.
+    as their correspondent RawClip's field. The RawClip is evaluated
+    to check if It's a highlight or a note. Finally, the RawClip is
+    passed to the 'parse_rawclip()' function and stored as a Clip in a
+    list to be returned as part or the output Extraction.
 
     An example of a raw kindle clip as seen in a 'My clippings.txt'
     file:
@@ -71,58 +75,66 @@ def parse_clips_file(file: str) -> Report:
 
     """
 
-    highlights: list[Highlight] = []
-    notes: int = 0
+    highlights: list[Clip]  = []
+    notes: list[Clip]       = []
     current_clip: list[str] = []
-    cnt: int = 1
+    cnt: int                = 1
 
     with open(file, mode='r', encoding='UTF-8', newline='\n') as f:
 
         for line in f:
 
-            if cnt >= 5: # The fifth line of a clip should be a delimiter
-                clip = Clip(current_clip[0], current_clip[1], current_clip[3])
+            # The fifth line of a clip should be a delimiter
+            if cnt >= 5:
+                cnt = 1
+                clip = parse_rawclip(RawClip(current_clip[0],
+                                             current_clip[1],
+                                             current_clip[2],
+                                             current_clip[3],
+                                             line.removesuffix('\n')))
 
-                if is_valid_clip(clip):
-                    highlights.append(parse_clip(clip))
+                if clip.type == 'highlight':
+                    highlights.append(clip)
                     current_clip.clear()
-                    cnt = 1
-
+                elif clip.type == 'note':
+                    notes.append(clip)
+                    current_clip.clear()
                 else:
-                    notes += 1
-                    current_clip.clear()
-                    cnt = 1
+                    raise RuntimeError
 
             else:
                 current_clip.append(line.removesuffix('\n'))
                 cnt += 1
 
         else:
-            return Report(notes, len(highlights), highlights)
+            return Extraction(highlights, notes)
 
-def parse_clip(clip: Clip) -> Highlight:
-    """Parse a Clip and generate a Highlight object.
+def parse_rawclip(rawclip: RawClip) -> Clip:
+    """Parse a RawClip and generate a Clip object.
 
     See 'parse_clips-file() documentation for more information about
     how Kindle stores the clips and how the parsing is done."""
+    return Clip(get_rawclip_type(rawclip),
+                rawclip.source,
+                parse_page_info(rawclip.info),
+                parse_location_info(rawclip.info),
+                parse_date_info(rawclip.info),
+                parse_time_info(rawclip.info),
+                rawclip.content)
 
-    return Highlight(clip.source,
-                     parse_page_info(clip.info),
-                     parse_location_info(clip.info),
-                     parse_date_info(clip.info),
-                     parse_time_info(clip.info),
-                     clip.content)
-
-def is_valid_clip(clip: Clip) -> bool:
-    """Check if a given Clip is a valid highlight.
-
-    For now it only checks if the clip is not a kindle note."""
-    return "Your Note" not in clip.info
+def get_rawclip_type(rclip: RawClip) -> str:
+    "Checks rclip type and return 'highlight' or 'note' string."
+    if  rclip.info.startswith('- Your Highlight'):
+        return 'highlight'
+    elif rclip.info.startswith('- Your Note'):
+        return 'note'
+    else:
+        raise RuntimeError('Your Kindle clips file looks weird. Couldn\'t parse it')
 
 def parse_page_info(string: str) -> list[int]:
     "Parse the page information as given in Clip.info."
-    match = re.search(r'pages? ([0-9]+)-([0-9]+)|page ([0-9]+)',
-                      string, re.IGNORECASE)
+    match = search(r'pages? ([0-9]+)-([0-9]+)|page ([0-9]+)',
+                      string, IGNORECASE)
     if match is None:
         return []
     else:
@@ -130,29 +142,29 @@ def parse_page_info(string: str) -> list[int]:
 
 def parse_location_info(string: str) -> list[int]:
     "Parse the location information as given in Clip.info."
-    match = re.search(r'Location ([0-9]+)-([0-9]+)|Location ([0-9]+)',
-                    string, re.IGNORECASE)
+    match = search(r'Location ([0-9]+)-([0-9]+)|Location ([0-9]+)',
+                    string, IGNORECASE)
     if match is None:
         return []
     else:
         return [int(loc) for loc in match.groups() if loc is not None]
 
-def parse_date_info(string: str) -> dt.date | None:
+def parse_date_info(string: str) -> date | None:
     "Parse the date information as given in Clip.info."
-    match = re.search(r'Added on [a-z]+, ([a-z]+) ([0-9]{1,2}), ([0-9]{4})',
-                      string, re.IGNORECASE)
+    match = search(r'Added on [a-z]+, ([a-z]+) ([0-9]{1,2}), ([0-9]{4})',
+                      string, IGNORECASE)
     if match is None:
         return None
     else:
         year: int  = int(match.group(3))
         month: int = MONTHS[match.group(1).lower()]
         day: int   = int(match.group(2))
-        return dt.date(year, month, day)
+        return date(year, month, day)
 
-def parse_time_info(string: str) -> dt.time | None:
+def parse_time_info(string: str) -> time | None:
     "Parse the time information as given in Clip.info."
-    match = re.search(r'([0-9]{1,2}):([0-9]{1,2}):([0-9]{1,2}) ([AMP]+)$',
-                      string, re.IGNORECASE)
+    match = search(r'([0-9]{1,2}):([0-9]{1,2}):([0-9]{1,2}) ([AMP]+)$',
+                      string, IGNORECASE)
     if match is None:
         return None
     else:
@@ -168,20 +180,20 @@ def parse_time_info(string: str) -> dt.time | None:
         elif period  == 'AM' and hour == 12:
             hour -= 12
 
-        return dt.time(hour, minutes, seconds)
+        return time(hour, minutes, seconds)
 
 ### IO
 ######################################################################
 
-def text_formatter(highlights: list[Highlight]) -> str:
+def text_formatter(highlights: list[Clip]) -> str:
     """Process a list of Highlights into a pretty text format."""
     return 'text formatter not implemented'
 
-def org_formatter(highlights: list[Highlight]) -> str:
+def org_formatter(highlights: list[Clip]) -> str:
     """Process a list of Highlights into a pretty org-mode format."""
     return 'org formatter not implemented'
 
-def json_formatter(highlights: list[Highlight]) -> str:
+def json_formatter(highlights: list[Clip]) -> str:
     """Process a list of Highlights into json format."""
     return 'json formatter not implemented'
 
@@ -210,21 +222,21 @@ args = parser.parse_args()
 
 if __name__ == '__main__':
 
-    report: Report = parse_clips_file(args.file)
+    extraction: Extraction = parse_rawclips_file(args.file)
 
     if not args.quiet:
-        print(f"Extracting highlights from '{args.file}'")
-        print(f"Found and discarded {report.notes_cnt} notes.")
-        print(f"Found and extracted {report.highlights_cnt} highlights.")
+        print(f"Extracting highlights from '{args.file}'.")
+        print(f"Found and discarded {len(extraction.notes)} notes.")
+        print(f"Found and extracted {len(extraction.highlights)} highlights.")
 
     if args.format == 'text':
-        output: str = text_formatter(report.highlights)
+        output: str = text_formatter(extraction.highlights)
     elif args.format == 'org':
-        output: str = org_formatter(report.highlights)
+        output: str = org_formatter(extraction.highlights)
     elif args.format == 'json':
-        output: str = json_formatter(report.highlights)
+        output: str = json_formatter(extraction.highlights)
     else:
-        raise RuntimeError
+        raise RuntimeError # Not necessary, but solves linter complains.
 
     if args.output_file is None:
         print(output)
@@ -235,45 +247,69 @@ if __name__ == '__main__':
 ### TESTS
 ######################################################################
 
-highlight_clip: Clip = Clip(
+import pytest
+
+highlight_rawclip: RawClip = RawClip(
     'Ciudad, urbanización y urbanismo en el siglo XX venezolano (Venezuela siglo XX nº 3) (Spanish Edition) (Almandoz Marte, Arturo)',
     '- Your Highlight on pages 40-45 | Location 542-546 | Added on Thursday, November 17, 2022 12:55:54 PM',
-    'La Venezuela que ingresa al siglo XX, asolada...'
-)
+    '',
+    'La Venezuela que ingresa al siglo XX, asolada...',
+    '==========')
 
-note_clip: Clip = Clip(
+note_rawclip: RawClip = RawClip(
     'Common LISP: A Gentle Introduction to Symbolic Computation (Dover Books on Engineering) (David S. Touretzky)',
     '- Your Note on page 217 | Location 3326 | Added on Friday, September 29, 2023 2:00:29 PM',
-    'Recursion en la vida real'
-)
-def test_is_valid_clip():
-    assert is_valid_clip(highlight_clip)
-    assert not is_valid_clip(note_clip)
+    '',
+    'Recursion en la vida real',
+    '==========')
 
-def test_parse_clip():
-    highlight = parse_clip(highlight_clip)
-    assert highlight.source    == highlight_clip.source
+corrupted_rawclip: RawClip = RawClip(
+    'Common LISP: A Gentle Introduction to Symbolic Computation (Dover Books on Engineering) (David S. Touretzky)',
+    '- Your Clip on page 217 | Location 3326 | Added on Friday, September 29, 2023 2:00:29 PM',
+    '',
+    'Recursion en la vida real',
+    '==========')
+
+def test_get_rawclip_type():
+    assert get_rawclip_type(highlight_rawclip) == 'highlight'
+    assert get_rawclip_type(note_rawclip) == 'note'
+    with pytest.raises(RuntimeError):
+        get_rawclip_type(corrupted_rawclip)
+
+def test_parse_rawclip():
+    highlight = parse_rawclip(highlight_rawclip)
+    assert highlight.source    == highlight_rawclip.source
     assert highlight.page      == [40, 45]
     assert highlight.location  == [542, 546]
-    assert highlight.date      == dt.date(2022, 11, 17)
-    assert highlight.time      == dt.time(12, 55, 54)
+    assert highlight.date      == date(2022, 11, 17)
+    assert highlight.time      == time(12, 55, 54)
     assert highlight.content   == 'La Venezuela que ingresa al siglo XX, asolada...'
 
-def test_parse_clips_file():
-    highlights = parse_clips_file('testing_clips.txt').highlights
-    assert all(map(lambda h: h.source != note_clip.source, highlights))
+def test_parse_rawclips_file():
+    testing_clips = parse_rawclips_file('testing_clips.txt')
+    # Tests for highlights extraction
+    highlights = testing_clips.highlights
+    assert all(map(lambda c: c.source != note_rawclip.source, highlights))
     assert len(highlights)         == 4
     assert highlights[0].page      == [40]
     assert highlights[0].location  == [542, 546]
     assert highlights[3].source    == 'Python (Python Documentation Authors)'
     assert highlights[3].content.startswith('To use formatted string literals')
-    assert highlights[3].date      == dt.date(2024, 5, 10)
-    assert highlights[3].time      == dt.time(18, 45, 50)
+    assert highlights[3].date      == date(2024, 5, 10)
+    assert highlights[3].time      == time(18, 45, 50)
+    # Tests for notes extraction
+    notes = testing_clips.notes
+    assert all(map(lambda c: c.source != highlight_rawclip.source, notes))
+    assert len(notes)         == 3
+    assert notes[0].page      == [217]
+    assert notes[0].location  == [3326]
+    assert notes[0].source.startswith('Common LISP:')
+    assert notes[0].content.startswith('Recursion en la vida real')
 
 def test_parse_time_info():
-    assert parse_time_info('11:59:59 PM') == dt.time(23, 59, 59)
-    assert parse_time_info('12:00:00 AM') == dt.time(0, 0, 0)
-    assert parse_time_info('12:00:01 AM') == dt.time(0, 0, 1)
-    assert parse_time_info('11:59:59 AM') == dt.time(11, 59, 59)
-    assert parse_time_info('12:00:00 PM') == dt.time(12, 0, 0)
-    assert parse_time_info('12:00:01 PM') == dt.time(12, 0, 1)
+    assert parse_time_info('11:59:59 PM') == time(23, 59, 59)
+    assert parse_time_info('12:00:00 AM') == time(0, 0, 0)
+    assert parse_time_info('12:00:01 AM') == time(0, 0, 1)
+    assert parse_time_info('11:59:59 AM') == time(11, 59, 59)
+    assert parse_time_info('12:00:00 PM') == time(12, 0, 0)
+    assert parse_time_info('12:00:01 PM') == time(12, 0, 1)
